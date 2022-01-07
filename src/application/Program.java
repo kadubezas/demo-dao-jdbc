9171ce7837c686dff367636768b79ee33b18bfcf
Eduardo Mesquita <kadubezas@gmail.com>
2022-01-07 13:27:27 -0300

    findAll implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,13 @@ public class Program {
 	    for(Seller s : list) {
 	    	System.out.println(s);
 	    }
+	    
+		System.out.println("\n=== Test 3: seller findAll ===");			
+	    list = sellerDao.findAll();
+	    
+	    for(Seller s : list) {
+	    	System.out.println(s);
+	    }
 	}
 
 }
