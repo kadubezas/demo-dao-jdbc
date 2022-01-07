7034afafa176f0acbb542d8d129584d497878f03
Eduardo Mesquita <kadubezas@gmail.com>
2022-01-07 11:29:26 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		Seller obj1 = new Seller(1,"BoB", "bob@gmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(obj1);
 	}
 
 }
